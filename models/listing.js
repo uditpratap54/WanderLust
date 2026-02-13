45834c30a8e4ca0312e42792a63d84ad151bae78
uditpratap54 <uditpratapsingh916@gmail.com>
2026-02-13 08:23:05 +0530

    second phase are start

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const review = require("./review");
+const { ref } = require("joi");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -19,7 +22,19 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: "Review",
+    }
+  ]
 });
 
+listingSchema.post("findOneAndDelete", async function (listing) {
+  if (listing) {
+    await Review.deleteMany({_id: { $in: listing.reviews } });
+  }
+  
+});
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
